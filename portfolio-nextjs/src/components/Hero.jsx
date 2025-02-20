2a2ab7dd5286a2588a863e13d2c1f3f859461a15
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2025-02-20 14:43:11 -0300

    updating hero phrase

--- portfolio-nextjs/src/components/Hero.jsx
+++ portfolio-nextjs/src/components/Hero.jsx
@@ -1,7 +1,18 @@
 export default function Hero() {
-    return (
-        <section style={{ padding: "20px", color: "#fff", background: "#000", textAlign: "center" }}>
-            <h3>Qualidade impulsionada por automação, métricas que direcionam decisões, soluções full stack que transformam código em inovação.</h3>
-        </section>
-    );
+  return (
+    <section
+      style={{
+        paddingTop: "80px",
+        paddingBottom: "20px",
+        color: "#fff",
+        background: "#000",
+        textAlign: "center",
+      }}
+    >
+      <h3>
+        Qualidade impulsionada por automação, métricas que direcionam decisões,
+        soluções full stack que transformam código em inovação.
+      </h3>
+    </section>
+  );
 }
