f4e092c3bb0dcb7004138d952f53c0943f14eca2
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2025-02-26 20:36:50 -0300

    fix navbar render button

--- portfolio-nextjs/src/components/Navbar.jsx
+++ portfolio-nextjs/src/components/Navbar.jsx
@@ -1,38 +1,9 @@
 "use client";
 
-import MenuIcon from "@mui/icons-material/Menu";
-import {
-  AppBar,
-  Box,
-  Button,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Navbar() {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const scrollToSection = (id) => {
-    const section = document.getElementById(id);
-    if (section) {
-      const offset = 50;
-      const top = section.offsetTop - offset;
-      window.scrollTo({ top, behavior: "smooth" });
-    }
-  };
-
-  const toggleDrawer = (open) => {
-    setMobileOpen(open);
-  };
-
   const navItems = [
     { label: "SOBRE MIM", id: "sobre-mim" },
     { label: "EXPERIÊNCIAS", id: "experiencias" },
@@ -41,63 +12,39 @@ export default function Navbar() {
   ];
 
   return (
-    <>
-      <AppBar
-        position="fixed"
-        sx={{ backgroundColor: "rgb(50, 50, 50)", boxShadow: "none" }}
-      >
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1, color: "white" }}>
-            Meu Portfólio
-          </Typography>
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: "rgb(50, 50, 50)",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* Texto "Meu Portfólio" some em telas menores que md */}
+        <Typography
+          variant="h6"
+          sx={{ color: "white", display: { xs: "none", md: "block" } }}
+        >
+          Meu Portfólio
+        </Typography>
 
-          <IconButton
-            edge="end"
-            color="inherit"
-            aria-label="menu"
-            sx={{ display: { xs: "block", md: "none" } }}
-            onClick={() => toggleDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-
-          {/* Menu Desktop */}
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            {navItems.map((item) => (
-              <Link key={item.id} href={`/#${item.id}`} passHref>
-                <Button sx={{ color: "white" }}>{item.label}</Button>
-              </Link>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-
-      <Drawer
-        anchor="right"
-        open={mobileOpen}
-        onClose={() => toggleDrawer(false)}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          "& .MuiDrawer-paper": { backgroundColor: "black" },
-        }}
-      >
-        <List sx={{ width: 250 }}>
+        {/* Botões responsivos */}
+        <Box>
           {navItems.map((item) => (
-            <ListItem key={item.id} disablePadding>
-              <ListItemButton
-                onClick={() => {
-                  scrollToSection(item.id);
-                  setMobileOpen(false);
+            <Link key={item.id} href={`/#${item.id}`} passHref>
+              <Button
+                sx={{
+                  color: "white",
+                  fontSize: { xs: "0.75rem", sm: "0.875rem", md: "1rem" }, // Ajusta o tamanho do texto nos botões
+                  padding: { xs: "4px 8px", sm: "6px 12px", md: "8px 16px" }, // Ajusta o padding
                 }}
               >
-                <ListItemText primary={item.label} sx={{ color: "white" }} />
-              </ListItemButton>
-            </ListItem>
+                {item.label}
+              </Button>
+            </Link>
           ))}
-        </List>
-      </Drawer>
-    </>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
